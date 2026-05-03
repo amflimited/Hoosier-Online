1e2855a3b2ad7bcdf1774cf94fb3b783877fe57b
amflimited <adam.ferree@gmail.com>
2026-05-03 14:13:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Hoosier Online</title>
+<title>Hoosier 
+    sOnline</title>
 
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
 
