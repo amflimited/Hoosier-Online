0a586e2733a92b5132610fbaf1c5d5e2215050ca
amflimited <adam.ferree@gmail.com>
2026-05-03 12:33:20 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>HoosierOnline</title>
+<title>Hoosier Online</title>
 
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
 
@@ -229,7 +229,7 @@ button:hover {
 
 <script>
 function checkout() {
-    window.location.href = "https://buy.stripe.com/YOUR_LINK";
+    window.location.href = "https://buy.stripe.com/eVqdR9ggibl26A833WgMw00";
 }
 </script>
 
